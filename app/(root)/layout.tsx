4411bc7012ac9615c1b38d5c70d7f7e961287016
unknown <botfinski@gmail.com>
2023-09-15 02:35:57 +0200

    onboarding, create thread, profile

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,11 +2,11 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 
-import "../globals.css";
 import Topbar from "@/components/shared/Topbar";
 import LeftSidebar from "@/components/shared/LeftSidebar";
 import RightSidebar from "@/components/shared/RightSidebar";
 import Bottombar from "@/components/shared/Bottombar";
+import "../globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
